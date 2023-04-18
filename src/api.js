0237e7d61783229635c912c46e604974b9491576
Gabriel Kozuki <gabrielkozuki@hotmail.com>
2023-04-18 10:41:03 -0300

    api changes + Row component

--- src/api.js
+++ src/api.js
@@ -1,29 +1,53 @@
-const API_KEY = ''
+const API_KEY = '038055597036fcb70b3cd616cc55c319'
 const API_DNS = 'https://api.themoviedb.org/3/'
 
-const categories = [
+export const categories = [
     {
         name: "trending",
-        title: "Em alta",
-        path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`
+        title: "Em Alta",
+        path: "/trending/all/week?api_key="+API_KEY+"&language=pt-BR",
+        isLarge: true,
     },
+    {
+        name: "netflixOriginals",
+        title: "Originais Netflix",
+        path: "/discover/tv?api_key="+API_KEY+"&with_networks=213",
+        isLarge: false,
+    },
+    {
+        name: "topRated",
+        title: "Populares",
+        path: "/movie/top_rated?api_key="+API_KEY+"&language=pt-BR",
+        isLarge: false,
+    },
+    {
+        name: "comedy",
+        title: "Comédias",
+        path: "/discover/tv?api_key="+API_KEY+"&with_genres=35",
+        isLarge: false,
+    },  
+    {
+        name: "romances",
+        title: "Romances",
+        path: "/discover/tv?api_key="+API_KEY+"&with_genres=10749",
+        isLarge: false,
+    },                
     {
         name: "documentaries",
         title: "Documentários",
-        path: `/discover/tv?api_key=${API_KEY}&with_genres=99`
+        path: "/discover/tv/api_key="+API_KEY+"&with_genres=99",
+        isLarge: false,
     }
 ]
 
-const getMovies = (cat) => {
-    let path_req = '';
-    
-    categories.forEach(el => {
-        if (el.name === cat) {
-            path_req = el.path
-        }
-    });
+export const getMovies = async(path) => {
+    try {
+        let url = API_DNS + path
+        const res = fetch(url)
 
-    return API_DNS + path_req
+        return (await res).json()
+    } catch(err) {
+        console.log(err);
+    }
 }
 
-export default getMovies
---
+++ src/components/Row.jsx
@@ -0,0 +1,46 @@
+import React, {useEffect} from   'react'
+import { getMovies } from '../api';
+import './Row.css'
+
+const imageHost = "https://image.tmdb.org/t/p/original";
+
+const Row = ({title, path, isLarge}) => {
+    const [movies, setMovies] = React.useState([])
+
+    const fetchMovies = async (_path) => {
+       try {
+        const data = await getMovies(_path);
+
+        setMovies(data?.results)
+        console.log(data)
+
+       } catch(error) {
+        console.log('error Row: ', error)
+       }
+
+    }
+
+    useEffect( () => {
+      fetchMovies(path)
+    }, [path] )
+
+    return (
+      <div className='row-container'>
+        <h2 className='row-header'>{title}</h2>
+          <div className='row-cards'>
+            {movies?.map( (movie) => {
+              return (
+                <img
+                  className={`movie-card ${isLarge && "movie-card-large"}` }
+                  key={movie.id} 
+                  src={imageHost + movie.poster_path} 
+                  alt={movie.name} >
+              </img>)
+            } )}
+          </div>
+      </div>
+    )
+
+}
+
+export default Row;
