650d2658e167b730fd48ec86054f6de7488da904
Gabriel Kozuki <gabrielkozuki@hotmail.com>
2023-04-24 14:15:38 -0300

    Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,41 +1,69 @@
-import { useState, useEffect } from 'react'
+import {useEffect, useState} from 'react'
 import './Banner.css'
+import {categories, getMovies } from '../api';
 
-import { categories, getMovies } from '../api'
+function Banner() {
+    const [movie, setMovie] = useState({});
 
-const Banner = () => {
-  const [movie, setMovie] = useState({})
+    const fetchRandomMovie = async () => {
+        try{
+            const netflixOriginalsCategory = categories.find( 
+                (category) => category.name === "netflixOriginals"
+            )
 
-  const fetchRandomMovie = async(_path) => {
-    try {
-      const netflixOriginalsCategory = categories.find((category) => category.name === 'netflixOriginals')
-      const data = await getMovies(netflixOriginalsCategory.path)
-      
-      const movies = data?.results
-      const randomIndex = Math.floor(Math.random() * movies.length)
-      setMovie(movies[randomIndex])
+            const data = await getMovies(netflixOriginalsCategory.path)
+            const movies = data?.results
+            const randomIndex = Math.floor(Math.random() * movies.length)
+            console.log(movies[randomIndex])
+            setMovie(movies[randomIndex])
 
-    } catch(err) {
-      console.log('error Banner: ', err)
+        }catch (error) {
+            console.log("Deu ruim no Banner.js", error)
+        }
     }
-  }
 
-  useEffect(() => {
-    fetchRandomMovie()
-  }, [])
 
-  return (
-    <header
-      className='banner-container'
-      style={{
-        backgroundSize: 'cover',
-        backgroundImage: ´url("https://image.tmdb.org/t/p/original" ${movie.backdrop_path})´ // fix
-      }}
-    >
-      <img src={"https://image.tmdb.org/t/p/original" + movie.poster_path} />
-    </header>
-  )
+    useEffect( () => {
 
+        fetchRandomMovie()
+
+    }, [] );
+
+
+    let truncate = (str, n) => {
+        return str?.length > n ? str?.substr(0, n-1) + '...' : str;
+    }
+
+    return (
+        <header
+            className='banner-container'
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+                roundPosition: "center-center",
+            }}
+        >
+            <div className='banner-content'>
+                <h1 className='banner-title'>
+                    { movie?.title || movie?.ñame || movie?.original_name }
+                </h1>
+                <div className='banner-button-container'>
+                    <div className='banner-button'>
+                        Assistir
+                    </div>
+                    <div className='banner-button'>
+                        Minha Lista
+                    </div> 
+                </div>
+               
+                <div className='banner-description'>
+                    <h2> {truncate(movie?.overview, 100 )} </h2>
+                </div>
+
+            </div>
+
+        </header>
+    )
 }
 
-export default Banner
+export default Banner;
